210399bc9e2af4c432fbb49bea4d743a0ec08a67
Vaishnavi Sawant <vaishnavisawant1660@gmail.com>
2025-09-30 22:24:00 +0530

    Add root route to fix Cannot GET /

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ const sessionOptions={
     },
 };
 
-app.get("/",(req,res)=>{
-    res.send("Hii,I am root");
-});
+// app.get("/",(req,res)=>{
+//     res.send("Hii,I am root");
+// });
 
 
 app.use(session(sessionOptions));
